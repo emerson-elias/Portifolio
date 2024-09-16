a02c19258c6dd41e884a43003a856da91a6861ba
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2024-09-16 11:58:03 -0300

    5.0

--- src/Components/tecnology/tecnology.jsx
+++ src/Components/tecnology/tecnology.jsx
@@ -1,159 +1,57 @@
-import '../tecnology/tecnology.scss'
+import React from 'react';
+import '../tecnology/tecnology.scss';
 
-
+const technologies = [
+  { name: 'HTML', img: '/icon-tec/html.png' },
+  { name: 'CSS', img: '/icon-tec/css.png' },
+  { name: 'SASS', img: '/icon-tec/sass.png' },
+  { name: 'REACT', img: '/icon-tec/reactt.png' },
+  { name: 'JAVASCRIPT', img: '/icon-tec/js.png' },
+  { name: 'TYPESCRIPT', img: '/icon-tec/ts.png' },
+  { name: 'PHP', img: '/icon-tec/php.png' },
+  { name: 'GIT', img: '/icon-tec/github.png' },
+  { name: 'VERCEL', img: '/icon-tec/vercel.png' },
+  { name: 'FIGMA', img: '/icon-tec/figma.png' },
+  { name: 'SQL', img: '/icon-tec/sql-server.png' },
+  { name: 'JAVA', img: '/icon-tec/java.png' },
+  { name: 'TAILWIND', img: '/icon-tec/tailwind.png' },
+  { name: 'BOOTSTRAP', img: '/icon-tec/bootstrap.png' },
+  { name: 'MATERIALIZE', img: '/icon-tec/materialize.png' },
+];
 
 export default function Tecnology() {
-    
-    return (
-        <section className="tecnology-container">
+  return (
+    <section className="tecnology-container">
 
-            <div className="tittle" id='tecnologias'>
-                <h1>skills</h1>
-                <span></span>
+      <div className="tittle" id='tecnologias'>
+        <h1>skills</h1>
+        <span></span>
+      </div>
+
+      <div className="row">
+
+        <div className="layers">
+
+          {technologies.map((tech, index) => (
+
+            <div key={index} className="card">
+
+              <div className="icon-tec">
+                <img src={tech.img} alt={tech.name} />
+              </div>
+
+              <div className="card-text">
+                <span>{tech.name}</span>
+              </div>
+
             </div>
 
-            <div className="row">
+          ))}
 
-                <div className="layers">
+        </div>
 
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="HTML" />
-                        </div>
-                        <div className="card-text">
-                            <span>html</span>
-                        </div>
-                    </div>
+      </div>
 
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="CSS" />
-                        </div>
-                        <div className="card-text">
-                            <span>CSS</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="SASS" />
-                        </div>
-                        <div className="card-text">
-                            <span>SASS</span>
-                        </div>
-                    </div>
-
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="REACT" />
-                        </div>
-                        <div className="card-text">
-                            <span>REACT</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="JS" />
-                        </div>
-                        <div className="card-text">
-                            <span>JAVASCRIPT</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="TS" />
-                        </div>
-                        <div className="card-text">
-                            <span>TYPESCRIPT</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="PHP" />
-                        </div>
-                        <div className="card-text">
-                            <span>PHP</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="GTI" />
-                        </div>
-                        <div className="card-text">
-                            <span>GIT</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="VERCEL" />
-                        </div>
-                        <div className="card-text">
-                            <span>VERCEL</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="FIGMA" />
-                        </div>
-                        <div className="card-text">
-                            <span>FIGMA</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="SQL" />
-                        </div>
-                        <div className="card-text">
-                            <span>SQL</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img alt="JAVA" />
-                        </div>
-                        <div className="card-text">
-                            <span>JAVA</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img alt="TAILWIND" />
-                        </div>
-                        <div className="card-text">
-                            <span>TAILWIND</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="BOOTSTRAP" />
-                        </div>
-                        <div className="card-text">
-                            <span>BOOTSTRAP</span>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="icon-tec">
-                            <img  alt="MATERIALIZE" />
-                        </div>
-                        <div className="card-text">
-                            <span>MATERIALIZE</span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-    )
+    </section>
+  );
 }
