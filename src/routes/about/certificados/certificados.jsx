625475a222e94ecfa3755cf5e723bd458dbe2f0a
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2024-10-03 02:19:16 -0300

    6.9

--- src/routes/about/certificados/certificados.jsx
+++ src/routes/about/certificados/certificados.jsx
@@ -1,94 +1,117 @@
-import '../certificados/certificados.scss'
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
-import MINERACAO from '/assets/certificados/certificado-mineração.png'
-import MONITOR from '/assets/certificados/certificado-monitor.png'
-import POTENCIALIZA from '/assets/certificados/certificado-potencializa.png'
-import DIPLOMA from '/assets/certificados/diploma.png'
+import '../certificados/certificados.scss';
+
+import MINERACAO from '/assets/certificados/certificado-mineração.png';
+import MONITOR from '/assets/certificados/certificado-monitor.png';
+import POTENCIALIZA from '/assets/certificados/certificado-potencializa.png';
+import DIPLOMA from '/assets/certificados/diploma.png';
 
 export default function Certificados() {
 
+    const containerRefs = useRef([])
+    const imgRefs = useRef([])
+
+    useEffect(() => {
+        const containers = containerRefs.current
+        const imgs = imgRefs.current
+
+        imgs.forEach((img) => {
+            gsap.set(img, {
+                opacity: 0,
+                scale: 0,
+            })
+        })
+
+        const handleMouseMove = (e, index) => {
+            const container = containers[index]
+            const img = imgs[index]
+
+            const rect = container.getBoundingClientRect()
+            const x = e.clientX - rect.left
+            const y = e.clientY - rect.top
+
+            const moveX = (x - img.offsetWidth / 2) * 0.1
+            const moveY = (y - img.offsetHeight / 2) * 0.1
+
+            gsap.to(img, {
+                x: moveX,
+                y: moveY,
+                duration: 0.5,
+                opacity: 1,  
+                scale: 1,    
+                ease: 'power1.out',
+            })
+        }
+
+        const handleMouseLeave = (index) => {
+            const img = imgs[index]
+            gsap.to(img, {
+                x: 0,
+                y: 0,
+                duration: 0.5,
+                opacity: 0, 
+                scale: 0,  
+                ease: 'power1.out',
+            })
+        }
+
+        containers.forEach((container, index) => {
+            if (container) {
+                container.addEventListener('mousemove', (e) => handleMouseMove(e, index))
+                container.addEventListener('mouseleave', () => handleMouseLeave(index))
+            }
+        })
+
+        return () => {
+            containers.forEach((container, index) => {
+                if (container) {
+                    container.removeEventListener('mousemove', (e) => handleMouseMove(e, index))
+                    container.removeEventListener('mouseleave', () => handleMouseLeave(index))
+                }
+            })
+        }
+    }, [])
+
     return (
         <section className="certificados-container">
- <div className="row">
+            <div className="word-big">
+                <span>certificado</span>
+                <span>dos</span>
+                <span>projetos</span>
+            </div>
 
-                    <div className="title">
-                        <h1>desafios aumentam</h1>
-                        <h1> conforme o progresso</h1>
-                    </div>
-
-                    <div className="layers">
+            <div className="row">
+                {[
+                    { title: 'mineração', img: MINERACAO, year: '2020' },
+                    { title: 'monitor', img: MONITOR, year: '2020' },
+                    { title: 'potencializa', img: POTENCIALIZA, year: '2020' },
+                    { title: 'diploma', img: DIPLOMA, year: '2021' },
+                ].map((project, index) => (
+                    <div
+                        key={index}
+                        className="layers"
+                        ref={(el) => (containerRefs.current[index] = el)}
+                    >
                         <div className="title-project">
-                            <h1>01 minimaslimo</h1>
+                            <h1>{project.title}</h1>
                         </div>
 
-                        <a className='svg' title="Acesse o projeto">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
-                                <path d="M90.54,0H89.46C71.61,23.72,49,47,27.43,65.3l.87,1.08c22-14.14,39.61-31.56,60.72-52C88.8,69,86.41,139.09,84.23,180H95.77c-2.18-40.92-4.79-111-5-165.64,20.89,19.81,38.75,37.66,60.94,52l.87-1.08C130.81,47.67,108.39,23.72,90.54,0Z" fill="currentColor" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="layers">
-                        <div className="title-project">
-                            <h1>02 speed food</h1>
+                        <div className="certi-icon">
+                            <img
+                                src={project.img}
+                                alt={project.title}
+                                ref={(el) => (imgRefs.current[index] = el)}
+                            />
                         </div>
 
-                        <a className='svg' title="Acesse o projeto">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
-                                <path d="M90.54,0H89.46C71.61,23.72,49,47,27.43,65.3l.87,1.08c22-14.14,39.61-31.56,60.72-52C88.8,69,86.41,139.09,84.23,180H95.77c-2.18-40.92-4.79-111-5-165.64,20.89,19.81,38.75,37.66,60.94,52l.87-1.08C130.81,47.67,108.39,23.72,90.54,0Z" fill="currentColor" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="layers">
-                        <div className="title-project">
-                            <h1>03 carros</h1>
+                        <div className="year">
+                            <h1>{project.year}</h1>
                         </div>
-
-                        <a className='svg' title="Acesse o projeto">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
-                                <path d="M90.54,0H89.46C71.61,23.72,49,47,27.43,65.3l.87,1.08c22-14.14,39.61-31.56,60.72-52C88.8,69,86.41,139.09,84.23,180H95.77c-2.18-40.92-4.79-111-5-165.64,20.89,19.81,38.75,37.66,60.94,52l.87-1.08C130.81,47.67,108.39,23.72,90.54,0Z" fill="currentColor" />
-                            </svg>
-                        </a>
                     </div>
-
-                    <div className="layers">
-                        <div className="title-project">
-                            <h1>04 natureza</h1>
-                        </div>
-
-                        <a className='svg' title="Acesse o projeto">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
-                                <path d="M90.54,0H89.46C71.61,23.72,49,47,27.43,65.3l.87,1.08c22-14.14,39.61-31.56,60.72-52C88.8,69,86.41,139.09,84.23,180H95.77c-2.18-40.92-4.79-111-5-165.64,20.89,19.81,38.75,37.66,60.94,52l.87-1.08C130.81,47.67,108.39,23.72,90.54,0Z" fill="currentColor" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="layers">
-                        <div className="title-project">
-                            <h1>05 isto é pousada</h1>
-                        </div>
-
-                        <a className='svg' title="Acesse o projeto">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
-                                <path d="M90.54,0H89.46C71.61,23.72,49,47,27.43,65.3l.87,1.08c22-14.14,39.61-31.56,60.72-52C88.8,69,86.41,139.09,84.23,180H95.77c-2.18-40.92-4.79-111-5-165.64,20.89,19.81,38.75,37.66,60.94,52l.87-1.08C130.81,47.67,108.39,23.72,90.54,0Z" fill="currentColor" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="layers">
-                        <div className="title-project">
-                            <h1>06 pablo</h1>
-                        </div>
-
-                        <a className='svg' title="Acesse o projeto">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
-                                <path d="M90.54,0H89.46C71.61,23.72,49,47,27.43,65.3l.87,1.08c22-14.14,39.61-31.56,60.72-52C88.8,69,86.41,139.09,84.23,180H95.77c-2.18-40.92-4.79-111-5-165.64,20.89,19.81,38.75,37.66,60.94,52l.87-1.08C130.81,47.67,108.39,23.72,90.54,0Z" fill="currentColor" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-           
+                ))}
+            </div>
         </section>
     )
 }
