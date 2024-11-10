99cab2588acb93306876c66b0f3515f12ed9a45c
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2024-11-10 03:49:22 -0300

    8.4

--- src/Components/modal/modal.jsx
+++ src/Components/modal/modal.jsx
@@ -1,10 +1,53 @@
 import '../modal/modal.scss'
 
-export default function Modal( { onClose } ){
-    return(
+export default function Modal({ onClose }) {
+    return (
         <section className="modal-container">
-            
-            <span class="iwwa--close" onClick={onClose}></span>
+
+          
+
+            <div className="closer">
+                <i className="fa-solid fa-xmark" onClick={onClose}></i>
+            </div>
+
+            <div className="row">
+                <div className="name-year-project">
+                    <h1>Em breve</h1>
+                    <span>01 de junho 2024</span>
+                </div>
+
+                <div className="box-description">
+
+                    <div className="layer-1">
+                        <h1>Tecnologias</h1>
+
+                        <span>01 HTML</span>
+                        <span>02 CSS</span>
+                        <span>03 SASS</span>
+                        <span>04 REACT</span>
+                        <span>05 GSAP</span>
+
+                        <div className="btn-projeto">
+                            <a href="">acessar</a>
+                        </div>
+
+                    </div>
+
+                    <div className="layer-2">
+
+                        <div className="box-1">
+                            <h1>Desafio</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia laborum neque ea, ad reiciendis ipsum quibusdam dolorum! Nam velit perspiciatis exercitationem error, aspernatur et. Aliquid quis accusamus tempore veniam fugiat!</p>
+
+                        </div>
+
+                        <div className="box-2">
+                            <h1>Solução</h1>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus velit exercitationem earum, nam reiciendis, ipsum numquam praesentium quos hic modi assumenda possimus eos ullam sapiente explicabo voluptatibus enim deserunt repellat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
