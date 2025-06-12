557c046bd4d44978fa16b319b09657182410895d
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-06-12 18:30:21 -0300

    17.6

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import "./global.scss"
 import { RouterProvider } from "react-router-dom"
 import rotas from "./routes"
 import Sound from "./components/global/sound/sound.jsx"
-import hook from "./hooks/mobile_view.js"
 
 function App() {
-    hook()
 
     return (
         <>
-            <section className="fixed-background-image"></section>
             <Sound />
             <RouterProvider router={rotas} />
         </>
