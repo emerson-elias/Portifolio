3478eecbc8693feeb65eaa8b94a252c650158a1b
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-06-12 17:47:09 -0300

    17.3

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import "./global.scss"
 import { RouterProvider } from "react-router-dom"
 import rotas from "./routes"
 import Sound from "./components/global/sound/sound.jsx"
+import hook from "./hooks/mobile_view.js"
 
 function App() {
+  hook()
+
   return (
     <>
       <Sound />
