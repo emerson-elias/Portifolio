48adb9016f42dd7545de5f31eb459e8094a83eac
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-06-12 10:00:53 -0300

    15.2

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
             ) : (
                 <>
                     <Cursor />
-                    <Nav_Bar />
+               
                     <Banner />
                     <Bio />
                     <Project />
