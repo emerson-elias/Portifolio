7e101812db326df26cec6d42e95fc902ae57dcbc
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-07-13 00:07:28 -0300

    22.9

--- src/components/global/sound/sound.jsx
+++ src/components/global/sound/sound.jsx
@@ -1,22 +1,24 @@
 import { useRef, useState, useEffect } from 'react'
 import { useAssetsLoaded } from '../../../contexts/AssetLoaderProvider'
+
 import styles from './sound.module.scss'
 import music from '/assets/music/sappheiros.mp3'
 
 export default function Sound() {
     const musicRef = useRef(null)
+    const spanRef = useRef(null)
+    
     const [soundState, setSoundState] = useState('OFF')
     const { allAssetsLoaded } = useAssetsLoaded()
 
     useEffect(() => {
-        // Só interage quando tudo estiver carregado
         if (allAssetsLoaded && musicRef.current) {
             musicRef.current.volume = 0.3
         }
     }, [allAssetsLoaded])
 
     const eventSound = () => {
-        if (!allAssetsLoaded) return // Não faz nada se não carregou
+        if (!allAssetsLoaded) return
 
         const musicElement = musicRef.current
         if (musicElement.paused) {
@@ -27,10 +29,34 @@ export default function Sound() {
         }
     }
 
+    useEffect(() => {
+        const root = document.getElementById('root')
+        const span = spanRef.current
+
+        const handleScroll = () => {
+            const scrollY = root.scrollTop
+            const scrollHeight = root.scrollHeight
+            const clientHeight = root.clientHeight
+
+            const distanceToBottom = scrollHeight - (scrollY + clientHeight)
+
+            if (distanceToBottom <= clientHeight / 2) {
+                span.classList.add(styles.colorBottom)
+            } else {
+                span.classList.remove(styles.colorBottom)
+            }
+        }
+
+        root.addEventListener('scroll', handleScroll)
+        return () => {
+            root.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
         <section className={styles.sound_container} title='Vamos aumentar a experiência?'>
             <a className={styles.sound} onClick={eventSound}>
-                <span>música : {soundState}</span>
+                <span ref={spanRef}>música : {soundState}</span>
                 <audio ref={musicRef} loop src={music} preload="auto"></audio>
             </a>
         </section>
--- src/components/global/topo/topo.jsx
+++ src/components/global/topo/topo.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import style from './topo.module.scss'
+import styles from './topo.module.scss'
 
 export default function Topo() {
     const topoRef = useRef(null)
@@ -8,31 +8,43 @@ export default function Topo() {
         const root = document.getElementById('root')
         const topo = topoRef.current
 
-        const handleScroll = () => {
-            if (root.scrollTop > 1000) {
+        const scroll = () => {
+            const scrollY = root.scrollTop
+            const scrollHeight = root.scrollHeight
+            const clientHeight = root.clientHeight
+
+            const distanceToBottom = scrollHeight - (scrollY + clientHeight)
+
+            if (scrollY > 200) {
                 topo.style.opacity = '1'
                 topo.style.pointerEvents = 'auto'
             } else {
                 topo.style.opacity = '0'
                 topo.style.pointerEvents = 'none'
             }
+
+            if (distanceToBottom <= clientHeight / 2) {
+                topo.classList.add(styles.colorBottom)
+            } else {
+                topo.classList.remove(styles.colorBottom)
+            }
         }
 
-        const handleClick = () => {
+        const click = () => {
             root.scrollTo({ top: 0, behavior: 'smooth' })
         }
 
-        root.addEventListener('scroll', handleScroll)
-        topo.addEventListener('click', handleClick)
+        root.addEventListener('scroll', scroll)
+        topo.addEventListener('click', click)
 
         return () => {
-            root.removeEventListener('scroll', handleScroll)
-            topo.removeEventListener('click', handleClick)
+            root.removeEventListener('scroll', scroll)
+            topo.removeEventListener('click', click)
         }
     }, [])
 
     return (
-        <section className={style.button_top} title="Volte ao topo" ref={topoRef}>
+        <section className={styles.button_top} title="Volte ao topo" ref={topoRef}>
             <a>
                 <i className="fa-solid fa-chevron-up"></i>
             </a>
