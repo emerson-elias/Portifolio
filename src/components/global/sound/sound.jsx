603ce1f507cea9871633ec5a6b6f704e1471b55d
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-09-03 15:40:12 -0300

    23.1

--- src/components/global/sound/sound.jsx
+++ src/components/global/sound/sound.jsx
@@ -7,7 +7,7 @@ import music from '/assets/music/sappheiros.mp3'
 export default function Sound() {
     const musicRef = useRef(null)
     const spanRef = useRef(null)
-    
+
     const [soundState, setSoundState] = useState('OFF')
     const { allAssetsLoaded } = useAssetsLoaded()
 
@@ -29,36 +29,10 @@ export default function Sound() {
         }
     }
 
-    useEffect(() => {
-        const root = document.getElementById('root')
-        const span = spanRef.current
-
-        const handleScroll = () => {
-            const scrollY = root.scrollTop
-            const scrollHeight = root.scrollHeight
-            const clientHeight = root.clientHeight
-
-            const distanceToBottom = scrollHeight - (scrollY + clientHeight)
-
-            if (distanceToBottom <= clientHeight / 2) {
-                span.classList.add(styles.colorBottom)
-            } else {
-                span.classList.remove(styles.colorBottom)
-            }
-        }
-
-        root.addEventListener('scroll', handleScroll)
-        return () => {
-            root.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
-
     return (
-        <section className={styles.sound_container} title='Vamos aumentar a experiência?'>
-            <a className={styles.sound} onClick={eventSound}>
-                <span ref={spanRef}>música : {soundState}</span>
-                <audio ref={musicRef} loop src={music} preload="auto"></audio>
-            </a>
-        </section>
+        <a className={styles.sound_container} onClick={eventSound} title='Vamos aumentar a experiência?'>
+            <span ref={spanRef}>música : {soundState}</span>
+            <audio ref={musicRef} loop src={music} preload="auto"></audio>
+        </a>
     )
 }
